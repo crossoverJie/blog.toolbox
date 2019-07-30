5cd098c1e6c7c50ff98f25ca459ef8bb299950f8
crossoverJie <crossoverjie@gmail.com>
2019-07-30 11:39:09 +0800

    :bug: Fixing a bug.

--- src/main/java/top/crossoverjie/nows/nows/service/impl/fixpic/IgnorePrefixFilterProcess.java
+++ src/main/java/top/crossoverjie/nows/nows/service/impl/fixpic/IgnorePrefixFilterProcess.java
@@ -12,7 +12,7 @@ import top.crossoverjie.nows.nows.filter.FilterProcess;
 public class IgnorePrefixFilterProcess implements FilterProcess {
     @Override
     public String process(String msg) {
-        if (msg != null && (msg.startsWith("https://ws") || msg.startsWith("http://ww")) ){
+        if (msg != null && (msg.startsWith("https://ws") || msg.startsWith("http://ww") || msg.startsWith("http://wx")) ){
             return msg ;
         }
         return null;
